9c3672200d098c4c7d9ec65f7543d6bd18a6536e
Xerach Casanova <xermam1984@hotmail.com>
2021-09-25 20:53:05 +0100

    prueba back

--- server.js
+++ server.js
@@ -43,7 +43,13 @@ app.use("/productos", productosRoute);
 app.use("/pedidos", pedidosRoute);
 
 
-const port = 3000;
+/*const port = 3000;
 const server = app.listen(port, () => {
     console.log('Servidor escuchando en el puerto --> '+ port);
-})
+})*/
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, err => {
+    if(err) throw err;
+    console.log("%c Server running", "color: green");
+});
